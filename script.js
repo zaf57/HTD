ed7408a3de1bfb16d3d2d08302efb5acc106c5be
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-08 01:41:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+gapi.load('client', initSheetsAPI);
+
+function initSheetsAPI() {
+  gapi.client.init({
+    apiKey: 'AIzaSyAIRpStG7VNS7XKpYmxunpO5hIsdZ_9d0E',
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(function() {
+    // API Google Sheets prête à être utilisée
+  });
+}
 function enregistrerValeurs() {
   var valeur1 = document.getElementById("valeur1").value;
   var valeur2 = document.getElementById("valeur2").value;
 
-  // Formatage des valeurs dans un format tabulaire (CSV)
-  var donnees = valeur1 + "," + valeur2 + "\n";
+  var spreadsheetId = 'hauteur site';
+  var range = 'Nom de la feuille!A1:B1';
+  var values = [
+    [valeur1, valeur2]
+  ];
 
-  // Enregistrement des données dans le stockage local
-  if (localStorage.donnees) {
-    localStorage.donnees += donnees;
-  } else {
-    localStorage.donnees = donnees;
-  }
-
-  // Réinitialisation des champs de saisie
-  document.getElementById("valeur1").value = "";
-  document.getElementById("valeur2").value = "";
-
-  alert("Valeurs enregistrées LOL mais où je sais pas!!");
+  gapi.client.sheets.spreadsheets.values.append({
+    spreadsheetId: spreadsheetId,
+    range: range,
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: values
+    }
+  }).then(function(response) {
+    console.log('Données enregistrées dans Google Sheets.');
+  }, function(error) {
+    console.error('Une erreur s\'est produite lors de l\'enregistrement des données dans Google Sheets:', error);
+  });
 }
