d4db11c2cd8768312fa26d8b937ede2275a12a8c
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-08 02:27:40 +0200

    Create script.js
    test

---
+++ script.js
@@ -0,0 +1,22 @@
+function enregistrerValeurs() {
+  // Récupérer les valeurs saisies dans le formulaire
+  var valeur1 = document.getElementById("valeur1").value;
+  var valeur2 = document.getElementById("valeur2").value;
+
+  // Créer un nouveau classeur Excel
+  var classeur = XLSX.utils.book_new();
+
+  // Créer une feuille de calcul
+  var feuille = XLSX.utils.json_to_sheet([
+    { Valeur1: valeur1, Valeur2: valeur2 }
+  ]);
+
+  // Ajouter la feuille de calcul au classeur
+  XLSX.utils.book_append_sheet(classeur, feuille, "Données");
+
+  // Générer le fichier Excel
+  var fichierExcel = XLSX.write(classeur, { bookType: "xlsx", type: "array" });
+
+  // Enregistrer le fichier Excel
+  saveAs(new Blob([fichierExcel], { type: "application/octet-stream" }), "donnees.xlsx");
+}
