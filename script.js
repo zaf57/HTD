7bf82950e881a20ddef365a971bc76c273d87796
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-09 16:25:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ gapi.load('client', initSheetsAPI);
 
 function initSheetsAPI() {
   gapi.client.init({
-    apiKey: 'AIzaSyAIRpStG7VNS7XKpYmxunpO5hIsdZ_9d0E',
+    apiKey: '578231543025-m9a7l9mqcti4d7i482rqnd5u9fgpt5ju.apps.googleusercontent.com',
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
   }).then(function() {
     // API Google Sheets prête à être utilisée
