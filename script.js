f818104050133e2e57bf8806f0c849d5d3653154
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-09 18:26:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function initSheetsAPI() {
     apiKey: '578231543025-m9a7l9mqcti4d7i482rqnd5u9fgpt5ju.apps.googleusercontent.com',
     clientId: 'Client_Web_1',
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    scope: 'https://www.googleapis.com/auth/spreadsheets'
+    scope: 'https://www.googleapis.com/auth/spreadsheets',
 
 client_id:"578231543025-m9a7l9mqcti4d7i482rqnd5u9fgpt5ju.apps.googleusercontent.com",
 project_id:"site-htd",
