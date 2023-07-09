ce55791b64fb31a1c8f5c7cfa6b743d1bf5d5af7
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-09 17:23:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function initSheetsAPI() {
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     scope: 'https://www.googleapis.com/auth/spreadsheets'
 
-"client_id":"578231543025-m9a7l9mqcti4d7i482rqnd5u9fgpt5ju.apps.googleusercontent.com",
-"project_id":"site-htd",
-"auth_uri":"https://accounts.google.com/o/oauth2/auth",
-"token_uri":"https://oauth2.googleapis.com/token",
-"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-"client_secret":"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",
-"redirect_uris":["https://zaf57.github.io/HTD/","https://zaf57.github.io"]
+client_id:"578231543025-m9a7l9mqcti4d7i482rqnd5u9fgpt5ju.apps.googleusercontent.com",
+project_id:"site-htd",
+auth_uri:"https://accounts.google.com/o/oauth2/auth",
+token_uri:"https://oauth2.googleapis.com/token",
+auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
+client_secret:"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",
+redirect_uris:["https://zaf57.github.io/HTD/","https://zaf57.github.io"]
     
   }).then(function() {
     // API Google Sheets prête à être utilisée
