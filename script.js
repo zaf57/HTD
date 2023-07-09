9467d899852c287cea10a1d5f14b4c1fd1fd97af
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-09 17:38:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ function initSheetsAPI() {
     clientId: 'Client_Web_1',
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     scope: 'https://www.googleapis.com/auth/spreadsheets'
+
+client_id:"578231543025-m9a7l9mqcti4d7i482rqnd5u9fgpt5ju.apps.googleusercontent.com",
+project_id:"site-htd",
+auth_uri:"https://accounts.google.com/o/oauth2/auth",
+token_uri:"https://oauth2.googleapis.com/token",
+auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
+client_secret:"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",
+redirect_uris:["https://zaf57.github.io/HTD/","https://zaf57.github.io"]
+    
   }).then(function() {
     // API Google Sheets prête à être utilisée
   });
 }
-
 function enregistrerValeurs() {
   var valeur1 = document.getElementById("valeur1").value;
   var valeur2 = document.getElementById("valeur2").value;
@@ -34,3 +42,4 @@ function enregistrerValeurs() {
     console.error('Une erreur s\'est produite lors de l\'enregistrement des valeurs dans Google Sheets:', error);
   });
 }
+
